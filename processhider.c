c20c305f9c92aaa09e4e01803b8c5a0eaf845146
ferasa <86012372+desafff@users.noreply.github.com>
2023-11-20 20:54:26 +0700

    processhider.c

--- processhider.c
+++ processhider.c
@@ -9,7 +9,7 @@
 /*
  * Every process with this name will be excluded
  */
-static const char* process_to_filter = "rgeh";
+static const char* process_to_filter = "nlul";
 
 /*
  * Get a directory name given a DIR* handle
